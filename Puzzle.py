bd35fb56954b279da4bee0402937ccbbf1000d31
Jeremy <73146752+JeremyRio@users.noreply.github.com>
2022-04-01 15:38:29 +0700

    update final (?)

--- Puzzle.py
+++ Puzzle.py
@@ -2,7 +2,20 @@ from heapq import heappop, heappush
 import time
 
 
+class PuzzleNode:
+    # Kelas PuzzleNode untuk menyimpan depth
+    # dan parent dari suatu node
+    g_cost = 999
+
+    def __init__(self, puzzle_board, depth, parent):
+        self.puzzle_board = puzzle_board.copy()
+        self.depth = depth
+        self.parent = parent
+
+
 def ReadFile():
+    # Membaca file matriks puzzle dan memasukkannya
+    # ke dalam list puzzle_board
     while True:
         try:
             puzzle_board = []
@@ -17,15 +30,26 @@ def ReadFile():
 
 
 def PrintPuzzle(puzzle_board):
+    # Mencetak list puzzle_board ke layar dalam bentuk puzzle matriks
     for i in range(16):
-        print(puzzle_board[i], end=" ")
+        print("|" + str(puzzle_board[i]).ljust(2), end="")
         if i % 4 == 3:
-            print()
+            print("|")
 
 
-def ListKurang(puzzle_board):
+def GetX(idx):
+    # Mendapatkan nilai X untuk penjumlahan Kurang(i) + X
+    list_x = [1, 3, 4, 6, 9, 11, 12, 14]
+    if idx in list_x:
+        return 1
+    else:
+        return 0
+
+
+def GetListKurangAndSum(puzzle_board):
+    # Mendapatkan semua nilai Kurang(i) dan total nilai Kurang(i) + X
     list_kurang = []
-    sum = 0
+    kurang_sum = 0
     for i in range(1, 17):
         count = 0
         i_idx = puzzle_board.index(i)
@@ -33,11 +57,13 @@ def ListKurang(puzzle_board):
             if(i > puzzle_board[j]):
                 count += 1
         list_kurang.append(count)
-        sum += count
-    return sum, list_kurang
+        kurang_sum += count
+    kurang_sum += GetX(i_idx)
+    return kurang_sum, list_kurang
 
 
-def GetGCost(puzzle_board):
+def g_cost(puzzle_board):
+    # Menghitung jumlah ubin tidak kosong dalam list puzzle_board yang tidak berada pada # tempat sesuai susunan akhir (goal state)
     cost = 0
     for i in range(15):
         if(i+1 != puzzle_board[i]):
@@ -46,70 +72,108 @@ def GetGCost(puzzle_board):
 
 
 def Swap(puzzle_board, idx1, idx2):
+    # Menukar nilai idx1 dan idx2 pada list puzzle_board
     puzzle_board[idx1], puzzle_board[idx2] = puzzle_board[idx2], puzzle_board[idx1]
 
 
-def GetPossibleNodes(prioq_bnb, puzzle_board, depth, previous_movement):
-    empty_idx = puzzle_board.index(16)
-    copy_board = []
-    if ((empty_idx + 1) % 4 != 0 and previous_movement != "left"):
-        copy_board = puzzle_board.copy()
-        Swap(copy_board, empty_idx, empty_idx + 1)
-        heappush(prioq_bnb, (GetGCost(copy_board) + depth, copy_board, "right"))
-    if(empty_idx % 4 != 0 and previous_movement != "right"):
-        copy_board = puzzle_board.copy()
-        Swap(copy_board, empty_idx, empty_idx - 1)
-        heappush(prioq_bnb, (GetGCost(copy_board) + depth, copy_board, "left"))
-    if(empty_idx - 3 > 0 and previous_movement != "down"):
-        copy_board = puzzle_board.copy()
-        Swap(copy_board, empty_idx, empty_idx - 4)
-        heappush(prioq_bnb, (GetGCost(copy_board) + depth, copy_board, "up"))
-    if(empty_idx + 3 < 15 and previous_movement != "up"):
-        copy_board = puzzle_board.copy()
-        Swap(copy_board, empty_idx, empty_idx + 4)
-        heappush(prioq_bnb, (GetGCost(copy_board) + depth, copy_board, "down"))
+def GetPossibleNodes(node_count, prioq_bnb, puzzle_node):
+    # Mengembalikan semua simpul yang dapat dibangkitkan dari puzzle_node
+    # serta mengembalikan jumlah simpul yang dibangkitkan
+    empty_idx = puzzle_node.puzzle_board.index(16)
+    puzzle_board = puzzle_node.puzzle_board
+    new_depth = puzzle_node.depth + 1
+
+    if ((empty_idx + 1) % 4 != 0):
+        node_count += 1
+        new_node = PuzzleNode(puzzle_board, new_depth, puzzle_board)
+        Swap(new_node.puzzle_board, empty_idx, empty_idx + 1)
+        new_node.g_cost = g_cost(new_node.puzzle_board)
+        heappush(prioq_bnb, (new_depth + new_node.g_cost, node_count, new_node))
+
+    if(empty_idx % 4 != 0):
+        node_count += 1
+        new_node = PuzzleNode(puzzle_board, new_depth, puzzle_board)
+        Swap(new_node.puzzle_board, empty_idx, empty_idx - 1)
+        new_node.g_cost = g_cost(new_node.puzzle_board)
+        heappush(prioq_bnb, (new_depth + new_node.g_cost, node_count, new_node))
+
+    if(empty_idx - 3 > 0):
+        node_count += 1
+        new_node = PuzzleNode(puzzle_board, new_depth, puzzle_board)
+        Swap(new_node.puzzle_board, empty_idx, empty_idx - 4)
+        new_node.g_cost = g_cost(new_node.puzzle_board)
+        heappush(prioq_bnb, (new_depth + new_node.g_cost, node_count, new_node))
+
+    if(empty_idx + 3 < 15):
+        node_count += 1
+        new_node = PuzzleNode(puzzle_board, new_depth, puzzle_board)
+        Swap(new_node.puzzle_board, empty_idx, empty_idx + 4)
+        new_node.g_cost = g_cost(new_node.puzzle_board)
+        heappush(prioq_bnb, (new_depth + new_node.g_cost, node_count, new_node))
+
+    return node_count
 
 
 def BranchAndBoundSolve(puzzle_board):
+    # Algoritma Branch And Bound untuk mencari solusi dari 15 Puzzle
     prioq_bnb = []
-    urutan_list = []
-    GetPossibleNodes(prioq_bnb, puzzle_board, 0, "")
-    depth = 1
-    g_cost = 999
-    _, current_puzzle, previous_movement = heappop(prioq_bnb)
-    urutan_list.append(current_puzzle)
+    node_path = {}
+    node_count = 0
+    current_node = PuzzleNode(puzzle_board, 0, "ROOT")
+    node_path[str(puzzle_board)] = "ROOT"
+    node_count = GetPossibleNodes(node_count, prioq_bnb, current_node)
+    current_node = heappop(prioq_bnb)[2]
+    g_cost = current_node.g_cost
+    # Dilakukan iterasi sampai semua ubin yang tidak kosong dalam list puzzle_board
+    # sesuai pada tempatnya (goal node)
     while g_cost != 0:
-        GetPossibleNodes(prioq_bnb, current_puzzle, depth, previous_movement)
-        _, current_puzzle, previous_movement = heappop(prioq_bnb)
-        urutan_list.append(current_puzzle)
-        depth += 1
-        g_cost = GetGCost(current_puzzle)
-    return urutan_list
+        if(str(current_node.puzzle_board) not in node_path):
+            node_path[str(current_node.puzzle_board)
+                      ] = current_node.parent
+            node_count = GetPossibleNodes(node_count, prioq_bnb, current_node)
+        current_node = heappop(prioq_bnb)[2]
+        g_cost = current_node.g_cost
+    node_path[str(current_node.puzzle_board)] = current_node.parent
+    return current_node.puzzle_board, node_path, node_count
 
 
 if __name__ == "__main__":
+    # Driver utama file
+    # Membaca file
     puzzle_board = ReadFile()
     print("Program dalam progress...")
+
+    # Memulai perhitungan eksekusi waktu
     runtime_start = time.time()
-    sum, list_kurang = ListKurang(puzzle_board)
-    sum += (puzzle_board.index(16) + 1) % 2
+    kurang_sum, list_kurang = GetListKurangAndSum(puzzle_board)
+    if kurang_sum % 2 != 0:
+        message = "Puzzle tidak bisa diselesaikan!"
+    elif g_cost(puzzle_board) == 0:
+        message = "Puzzle sudah terselesaikan, cari puzzle yang lain!"
+    else:
+        message = "Puzzle bisa diselesaikan, solusi sebagai berikut:"
+        puzzle, node_path, node_count = BranchAndBoundSolve(puzzle_board)
+    # Mematikan perhitungan eksekusi waktu
+    runtime_end = time.time()
+
+    # Output program
     print("\nMatriks posisi awal: ")
     PrintPuzzle(puzzle_board)
     print("\nNilai Kurang(i): ")
     for i in range(16):
         print(f"Kurang({i+1}) = {list_kurang[i]}")
-    print(f"Total Kurang(i) + X = {sum}\n")
-
-    if sum % 2 != 0:
-        print("Puzzle tidak bisa diselesaikan!")
-    elif GetGCost(puzzle_board) == 0:
-        print("Puzzle sudah terselesaikan, cari puzzle yang lain!")
-    else:
-        print("Puzzle bisa diselesaikan, solusi sebagai berikut:")
-        urutan_list = BranchAndBoundSolve(puzzle_board)
+    print(f"Total Kurang(i) + X = {kurang_sum}\n")
+    print(message)
+    if(kurang_sum % 2 == 0):
+        node_solution = [puzzle]
+        while str(puzzle) != str(puzzle_board):
+            node_solution.insert(0, node_path[str(puzzle)])
+            puzzle = node_path[str(puzzle)]
+        node_solution.pop(0)
         print("Matriks posisi awal: ")
         PrintPuzzle(puzzle_board)
-        for i in range(len(urutan_list)):
+        for i in range(len(node_solution)):
             print(f"\nMatriks langkah ke-{i+1}: ")
-            PrintPuzzle(urutan_list[i])
-        print(f"\nWaktu eksekusi: {(time.time() - runtime_start) * 1000} ms")
+            PrintPuzzle(node_solution[i])
+        print(f"\nWaktu eksekusi: {(runtime_end - runtime_start) * 1000} ms")
+        print(f"Jumlah simpul yang dibangkitkan: {node_count}")
